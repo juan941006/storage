bae574f5c0722b1ef2c39602ed3a4d6c91bc0dff
juanf <jfgutierrez@correo.udistrital.edu.co>
2022-07-24 18:03:21 -0500

    se intregra el aside del carrito de compras y se efectua la logica para cerrar los menus abiertos

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail          = document.querySelector('.navbar-email');
+const menuBurguer        = document.querySelector('.menu');
+const menuShoppingCart   = document.querySelector('.navbar-shopping-cart');
+const desktopMenu        = document.querySelector('.desktop-menu');
+const mobileMenu         = document.querySelector('.mobile-menu');
+const aside              = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuBurguer.addEventListener('click',toggleMobileMenu);
+menuShoppingCart.addEventListener('click',toggleShoppingCart);
+
 
 function toggleDesktopMenu(){
-  desktopMenu.classList.toggle('inactive')
+  mobileMenu.classList.add('inactive');
+  aside.classList.add('inactive');
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+  aside.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart(){
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive'); 
+  aside.classList.toggle('inactive');
 }
